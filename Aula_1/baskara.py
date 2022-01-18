d653300eed353374a43a24e4827ec78d352a1fe6
HugoXR <rodrigo.x.viera.guesuh@gmail.com>
2022-01-18 17:41:09 -0300

    Programa para resolucao de equacao do segundo grau versao final

--- Aula_1/baskara.py
+++ Aula_1/baskara.py
@@ -2,21 +2,26 @@
 from math import sqrt
 
 text = input("Insira os valores de a, b e c da equacao separados por virgula: ")
-
-a, b, c = tuple(int(value) for value in text.split(","))
+try:
+	a, b, c = tuple(int(value) for value in text.split(","))
+except ValueError:
+	print("Os valores das variaveis nao sao compativeis")
 
 def solucao_eq_segundo_grau(a, b, c):
 	delta = b**2 - 4*a*c
-
+	
 	if(delta < 0):
-		print("Não há solução real")
-		return 
+		raise ValueError("Nao ha solucao real")
+	
 	x_1 = (-b+sqrt(delta))/(2*a)
 	x_2 = (-b-sqrt(delta))/(2*a) 
 
 	return x_1, x_2
 
-x_1, x_2 = solucao_eq_segundo_grau(a,b,c)
-
-print(f"As solucoes desta equacao sao x_1 = {x_1:.2f} e x_2 = {x_2:.2f}")
-
+try:
+	x_1, x_2 = solucao_eq_segundo_grau(a,b,c)
+	print(f"As solucoes desta equacao sao x_1 = {x_1:.2f} e x_2 = {x_2:.2f}")
+except ValueError as error:
+	print(error)
+except NameError as nerror:
+	print(nerror)	
