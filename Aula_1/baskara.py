4cc4a2d5ee6cccf99a27ae3664fcd84f4b4d298f
HugoXR <rodrigo.x.viera.guesuh@gmail.com>
2022-01-18 17:18:10 -0300

    Programa para resolucao de equacao do segundo grau

---
+++ Aula_1/baskara.py
@@ -0,0 +1,22 @@
+#Resolvendo equação de segundo grau
+from math import sqrt
+
+text = input("Insira os valores de a, b e c da equacao separados por virgula: ")
+
+a, b, c = tuple(int(value) for value in text.split(","))
+
+def solucao_eq_segundo_grau(a, b, c):
+	delta = b**2 - 4*a*c
+
+	if(delta < 0):
+		print("Não há solução real")
+		return 
+	x_1 = (-b+sqrt(delta))/(2*a)
+	x_2 = (-b-sqrt(delta))/(2*a) 
+
+	return x_1, x_2
+
+x_1, x_2 = solucao_eq_segundo_grau(a,b,c)
+
+print(f"As solucoes desta equacao sao x_1 = {x_1:.2f} e x_2 = {x_2:.2f}")
+
