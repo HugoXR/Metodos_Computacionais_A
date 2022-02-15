4071a03dd4e2a2531be6a385abbbc7a187d73793
HugoXR <rodrigo.x.viera.guesuh@gmail.com>
2022-02-15 18:55:20 -0300

    Exercicio 5.22 do Livro A Primer on Scientific Programming with Python

--- Exercicios/position2velocity.py
+++ Exercicios/position2velocity.py
@@ -14,15 +14,17 @@ v_x = (x[1:] - x[:-1]) / s
 v_y = (y[1:] - y[:-1]) / s
 
 plt.plot(x, y, "-or")
-plt.show()
+plt.savefig("Grafico y(x).pdf")
 plt.close("all")
 
 plt.plot(time_k, v_y, "-ob")
 plt.xlabel("Time (s)")
 plt.ylabel("Velocity in y (m/s)")
 
+
 plt.twinx()
 plt.plot(time_k, v_x, "-or")
+plt.ylim(0, np.max(v_x))
 plt.ylabel("Veloity in x (m/s)")
-plt.show()
+plt.savefig("Grafico V_x(t) e V_y(t).pdf")
 plt.close('all')
