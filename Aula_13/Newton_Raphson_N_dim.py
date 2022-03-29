90e8f71cf662a0a4cef81370eba71fdde06f1bf3
HugoXR <rodrigo.x.viera.guesuh@gmail.com>
2022-03-29 15:37:01 -0300

    Equacao nao linear

--- Aula_13/Newton_Raphson_N_dim.py
+++ Aula_13/Newton_Raphson_N_dim.py
@@ -1,15 +1,14 @@
 # Resolucao de sistemas nao lineares
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 
 
 def F(X):
     return np.array([np.sin(X[0]**2 + X[1]**2), 2*X[0] - X[1] - 1/(X[0] + X[1])])
 
 
-nplot = 200
-lim_plot = np.array([[0, 3], [0, 3]])
+nplot = 50
+lim_plot = np.array([[-3.00001, 3], [-3.0001, 3]])
 xplot = np.repeat(np.linspace(lim_plot[0, 0], lim_plot[0, 1], nplot), nplot).reshape([nplot, nplot]).T
 yplot = np.repeat(np.linspace(lim_plot[1, 0], lim_plot[1, 1], nplot), nplot).reshape([nplot, nplot])
 zplot = F([xplot, yplot])
@@ -32,22 +31,40 @@ def J(X):
 
 
 def Newton_Raphson(F, J, X0, erroMaximo=1E-10):
+    X = X0.copy()
     erro = 1E10
-    passos = np.array([X0])
+    passos = np.array([X])
     n = 0
-    while(erro > erroMaximo) and (n<100):
+    while(erro > erroMaximo) and (n < 100):
         n += 1
-        delta_x = np.linalg.solve(J(X0), -F(X0))
-        X0 += delta_x
-        passos = np.vstack([passos, X0])
+        delta_x = np.linalg.solve(J(X), -F(X))
+        X += delta_x
+        passos = np.vstack([passos, X])
         erro = np.abs(delta_x).max()
-    return X0, passos
+    return X, passos
 
-
+g = 10
 fig.add_subplot(1, 3, 3)
-X0 = np.array([0.5, 1])
-X1, passos = Newton_Raphson(F, J, X0)
-plt.plot(passos[:, 0], passos[:, 1], '-o')
+cm = plt.get_cmap('gist_ncar')
+imagem = np.zeros([nplot, nplot, 4])
+zerosF = np.array([[0, 0]])
+for i in range(nplot):
+    for j in range(nplot):
+        X0 = np.array((xplot[i, j], yplot[i, j]))
+        X1, passos = Newton_Raphson(F, J, X0)
+        zerosF = np.vstack([zerosF, X1])
+        gamma = X1[0]+X1[1]
+        cor = cm(max(min((gamma+g)/2/g, 1), 0))
+        imagem[i, j, :] = cor
+        #plt.plot(passos[[0, -1], [0, 0]], passos[[0, -1], [1, 1]], '-', color=cor)
+        #plt.plot(X0[0], X0[1], 's', color=cor)
+        #plt.plot(X1[0], X1[1], '*', color=cor)
+        #print(gamma, X0, X1, F(X1))
+
+plt.imshow(imagem[-1:1:-1, :], extent=(lim_plot[0][0], lim_plot[0][1], lim_plot[1][0], lim_plot[1][1]))
+plt.plot(zerosF[:, 0], zerosF[:, 1], 'ok')
+plt.xlim(lim_plot[0, 0], lim_plot[0, 1])
+plt.ylim(lim_plot[1, 0], lim_plot[1, 1])
 plt.xlabel('x')
 plt.ylabel('y')
 
